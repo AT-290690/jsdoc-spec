7bc019e6af1e0f2f717557d03b2bfb205ee2f45c
anthony163 <anthony@techpods.co>
2023-04-02 15:39:00 +0300

    Use constructor name for equality of constructors

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 const { readFile } = require('fs/promises')
 const { normalize } = require('path')
 const { runInContext, createContext } = require('vm')
-const __equal = require('fast-deep-equal')
+const __equal = (a, b) => {
+  if (a === b) return true
+  if (a && b && typeof a == 'object' && typeof b == 'object') {
+    if (a.constructor.name !== b.constructor.name) return false
+    let length, i, keys
+    if (Array.isArray(a)) {
+      length = a.length
+      if (length != b.length) return false
+      for (i = length; i-- !== 0; ) if (!__equal(a[i], b[i])) return false
+      return true
+    }
+    if (a instanceof Map && b instanceof Map) {
+      if (a.size !== b.size) return false
+      for (i of a.entries()) if (!b.has(i[0])) return false
+      for (i of a.entries()) if (!__equal(i[1], b.get(i[0]))) return false
+      return true
+    }
+    if (a instanceof Set && b instanceof Set) {
+      if (a.size !== b.size) return false
+      for (i of a.entries()) if (!b.has(i[0])) return false
+      return true
+    }
+    if (ArrayBuffer.isView(a) && ArrayBuffer.isView(b)) {
+      length = a.length
+      if (length != b.length) return false
+      for (i = length; i-- !== 0; ) if (a[i] !== b[i]) return false
+      return true
+    }
+    if (a.constructor === RegExp)
+      return a.source === b.source && a.flags === b.flags
+    if (a.valueOf !== Object.prototype.valueOf)
+      return a.valueOf() === b.valueOf()
+    if (a.toString !== Object.prototype.toString)
+      return a.toString() === b.toString()
+    keys = Object.keys(a)
+    length = keys.length
+    if (length !== Object.keys(b).length) return false
+    for (i = length; i-- !== 0; )
+      if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false
+    for (i = length; i-- !== 0; ) {
+      let key = keys[i]
+      if (!__equal(a[key], b[key])) return false
+    }
+    return true
+  }
+  return a !== a && b !== b
+}
 
 const formatPerf = (time) => {
   const isSec = time[1] > 1000000
